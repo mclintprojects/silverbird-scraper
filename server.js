6303c94714220227b407507f514717016b1e846d
Glitch (silverbird-scraper) <none>
2018-05-28 14:03:39 +0000

    👠🐥 Checkpoint ./server.js:815292/831 ./package.json:815292/1437

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 var express = require('express');
-var request = require('request')
+var request = require('request-promise')
 var cors = require('cors')
 var cheerio = require('cheerio')
 var app = express();
+var url = 'https://silverbirdcinemas.com/cinema/accra/';
 
 app.use(cors())
 
-app.get('/movies', (req, response) => {
-  
+app.get('/movies', async function(req, response){
+  try{
+    let html = await request(url);
+    const $ = cheerio.load(html);
+    let movies = [];
+    
+    $('.entry-item').each(function(index, element){
+      let movie = {};
+      movie.thumbnail = $(this).children('.entry-thumb img').attr('src')
+      
+      movies.push(movie)
+    });
+    
+    response.send({movies});
+  }
+  catch(error){
+    response.status(500).send({error});
+  }
 });
 
 var listener = app.listen(process.env.PORT, function () {
